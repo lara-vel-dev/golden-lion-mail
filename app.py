7b36990edb1edb8bb696797e891f139571f254e6
Daniela Lara <laradni@icloud.com>
2023-10-24 13:28:33 -0600

    (Mod) Main files

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html')
+
+
+@app.route('/main-program')
+def main_program():
+    return render_template('main-program.html')
 
 
 if __name__ == '__main__':
